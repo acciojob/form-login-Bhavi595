1fa601c68fc127c265e7a7710c34242e04e7854e
Bhavi <129272547+Bhavi595@users.noreply.github.com>
2023-09-25 11:14:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getFormvalue() {
     //Write your code here
-	let x = docoment.querySelectall("form>input");
-	alert(x[0].innertext +x x.[1].innertext );
+	let x = document.querySelectorAll("form>input");
+	alert(x[0].value + x[1].value );
 	
 
 }
